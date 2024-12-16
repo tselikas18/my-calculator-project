0fcbbd5145942fc9f973f1529a7c34e31827502c
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2024-12-16 19:42:25 +0200

    project created

---
+++ src/gr/aueb/cf/ch2/SubApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch2;
+
+public class SubApp {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        int result = 0;
+
+        result = sub(num1, num2);
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+}
